e110409d1fef916ae1cc08f5809bef6a5ea30918
asritha26k <132151227+asritha26k@users.noreply.github.com>
2025-02-16 13:04:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import requests
+
+app = FastAPI()
+
+# Mount static files for CSS & JS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up templates directory
+templates = Jinja2Templates(directory="templates")
+
+API_URL = "https://positiveheadlinegenerator.onrender.com/predict/"
+
+@app.get("/")
+async def serve_homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/predict/")
+async def get_prediction(text: str):
+    response = requests.post(API_URL, json={"text": text})
+    return response.json()
